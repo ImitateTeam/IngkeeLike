70f42522f489acb488e779fa8134094f563c78a6
alo0423 <771761134@qq.com>
2017-01-12 11:53:32 +0800

    试着添加热门页，修改logo

---
+++ app/src/main/java/com/live/ingkeelike/ui/home/fragment/hotfragment/presenter/IHotPresenter.java
@@ -0,0 +1,12 @@
+package com.live.ingkeelike.ui.home.fragment.hotfragment.presenter;
+
+/**
+ * Created by alo on 2017/1/10.
+ */
+
+public interface IHotPresenter {
+    /**
+     * 加载数据
+     */
+    void doLoadData(String path);
+}
