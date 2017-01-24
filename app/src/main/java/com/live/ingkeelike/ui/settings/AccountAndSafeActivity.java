efc94fadd70a610bbe6ed5006ee429fbc88b1826
android-json <846774409@qq.com>
2017-01-24 16:34:39 +0800

    json_dev

---
+++ app/src/main/java/com/live/ingkeelike/ui/settings/AccountAndSafeActivity.java
@@ -0,0 +1,15 @@
+package com.live.ingkeelike.ui.settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.live.ingkeelike.R;
+
+public class AccountAndSafeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_and_safe);
+    }
+}
